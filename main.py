6a0450f5de5d70db4a847e2c3f9de347d86f91f8
Matheus <matheushe13d@gmail.com>
2026-05-15 21:24:51 -0300

    ADD: Adicionado sistema de coleta de dados

--- main.py
+++ main.py
@@ -8,10 +8,26 @@ saudacoes = [
     'Saudações guerreiro!',
     'Eae, gostaria de adentrar no sistema?'
 ]
+def inputer(frase, tamanho=3):
+    _temp = input(frase)
+    if len(_temp) >= tamanho:
+        return _temp
+    else:
+        print('Valor inválido, preciso de no mínimo o tamanho igual ou maior a', tamanho)
+        return inputer(frase, tamanho)
 
 def saudacao():
     print(random.choice(saudacoes))
-
+    
+def coletar_usuario():
+    username = inputer("Insira seu nome de usuário: ")
+    return username
+def coletar_senha():
+    password_hash = hashlib.sha256(inputer("Shhhh... Insira sua senha: ", tamanho=8).encode()).hexdigest()
+    return password_hash
+def coletar_nome():
+    nome = inputer('Hmmm. E seu nome, qual seria?')
+    return nome
 def inicializar():
     global _database
     _database = MySQL('./db.db')
